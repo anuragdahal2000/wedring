323785860b03cefeb65b6499a7c513918b0e9aa3
anuragdahal2000 <anuragdahal2000>
2023-06-16 17:25:37 +0545

    update new routes

--- lib/router.dart
+++ lib/router.dart
@@ -1,15 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:wedring/screens/Info/basic_info.dart';
+import 'package:wedring/screens/Info/edu_info.dart';
+import 'package:wedring/screens/Info/intrests.dart';
+import 'package:wedring/screens/Info/profile_info.dart';
 import 'package:wedring/screens/auth/forget_password.dart';
 import 'package:wedring/screens/auth/new_password.dart';
 import 'package:wedring/screens/auth/opt_verification.dart';
 import 'package:wedring/screens/auth/sign_in.dart';
-import 'package:wedring/screens/auth/sign_up.dart';
-import 'package:wedring/screens/chat/chat.dart';
+import 'package:wedring/screens/chat/chat_list.dart';
+import 'package:wedring/screens/signup/sign_up.dart';
+import 'package:wedring/screens/chat/single_chat.dart';
 import 'package:wedring/screens/home/scaffold_with_bottom_nav.dart';
 import 'package:wedring/screens/matches/match.dart';
+import 'package:wedring/screens/onboarding/onboard.dart';
 import 'package:wedring/screens/profile/profile.dart';
 import 'package:wedring/screens/settings/settings.dart';
+import 'package:wedring/screens/signup/sign_up_2.dart';
 import 'package:wedring/util/helper.dart';
 
 import 'screens/home/home.dart';
@@ -20,13 +27,21 @@ class AppRouter {
   static GoRouter router = GoRouter(
     navigatorKey: AppManager.navigatorKey,
     debugLogDiagnostics: true,
-    initialLocation: "/profile",
+    initialLocation: "/home",
     errorBuilder: (context, state) => Scaffold(
       body: Center(
         child: Text(state.error.toString()),
       ),
     ),
     routes: <RouteBase>[
+      GoRoute(
+        name: 'onboard',
+        path: '/onboard',
+        pageBuilder: (context, state) => FadeTransitionPage(
+          key: state.pageKey,
+          child: const OnboardScreen(),
+        ),
+      ),
       GoRoute(
         name: 'signin',
         path: '/signin',
@@ -60,6 +75,41 @@ class AppRouter {
         name: 'signup',
         path: '/signup',
         builder: (context, state) => const SignUp(),
+        routes: [
+          GoRoute(
+            name: 'signup-2',
+            path: 'signup-2',
+            builder: (context, state) => const SignUp2(),
+            routes: [
+              GoRoute(
+                name: 'basic-info',
+                path: 'basic-info',
+                builder: (context, state) => const BasicInfo(),
+                routes: [
+                  GoRoute(
+                    name: 'edu-info',
+                    path: 'edu-info',
+                    builder: (context, state) => const EduInfo(),
+                    routes: [
+                      GoRoute(
+                        name: 'profile-info',
+                        path: 'profile-info',
+                        builder: (context, state) => const ProfileInfo(),
+                        routes: [
+                          GoRoute(
+                            name: 'interests',
+                            path: 'interests',
+                            builder: (context, state) => const IntrestScreen(),
+                          ),
+                        ],
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            ],
+          )
+        ],
       ),
       ShellRoute(
         navigatorKey: _shellNavigatorKey,
@@ -85,14 +135,14 @@ class AppRouter {
             name: 'chat',
             path: '/chat',
             pageBuilder: (context, state) => const NoTransitionPage(
-              child: Chat(),
+              child: ChatList(),
             ),
           ),
           GoRoute(
             name: 'profile',
             path: '/profile',
             pageBuilder: (context, state) {
-              return const NoTransitionPage(
+              return NoTransitionPage(
                 child: Profile(),
               );
             },
@@ -105,6 +155,11 @@ class AppRouter {
             ],
           ),
         ],
+      ),
+      GoRoute(
+        name: 'single-chat',
+        path: '/single-chat',
+        builder: (context, state) => const SingleChat(),
       ),
     ],
   );
