3bc7ce10e9a034da0196be996c9a418236635bb7
anuragdahal2000 <be2018se626@gmail.com>
2023-07-02 20:29:37 +0545

    firebase init

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:wedring/router.dart';
-import 'package:wedring/util/theme.dart';
+import 'package:wedring/utils/theme.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(
     const MyApp(),
   );
