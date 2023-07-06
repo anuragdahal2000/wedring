76c939c4d204a9eec5093905bd6b257bad1da5f0
anuragdahal2000 <be2018se626@gmail.com>
2023-07-06 08:59:34 +0545

    firebase init with options

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:wedring/controllers/auth.dart';
 import 'package:wedring/router.dart';
 import 'package:wedring/utils/theme.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(
-    const MyApp(),
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (_) => AuthController(),
+        ),
+      ],
+      child: const MyApp(),
+    ),
   );
 }
 
