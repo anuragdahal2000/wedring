454a433de9194b4f67c6a646cb7afdffb2587760
anuragdahal2000 <be2018se626@gmail.com>
2023-07-06 10:51:50 +0545

    ui update

--- lib/screens/profile/profile.dart
+++ lib/screens/profile/profile.dart
@@ -26,126 +26,125 @@ class _ProfileState extends State<Profile> {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        CachedNetworkImage(
-          imageUrl: currentUser.photoURL!,
-          imageBuilder: (context, imageProvider) => Container(
-            margin: const EdgeInsets.only(top: 20, bottom: 20),
-            height: 200,
-            width: double.infinity,
-            decoration: BoxDecoration(
-              color: Colors.grey[300],
-              borderRadius: BorderRadius.circular(10),
-              image: DecorationImage(
-                image: imageProvider,
-                fit: BoxFit.cover,
+    return SingleChildScrollView(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          CachedNetworkImage(
+            imageUrl: currentUser.photoURL!,
+            imageBuilder: (context, imageProvider) => Container(
+              margin: const EdgeInsets.only(top: 20, bottom: 20),
+              height: 200,
+              width: double.infinity,
+              decoration: BoxDecoration(
+                color: Colors.grey[300],
+                borderRadius: BorderRadius.circular(10),
+                image: DecorationImage(
+                  image: imageProvider,
+                  fit: BoxFit.cover,
+                ),
               ),
             ),
+            placeholder: (context, url) => const Center(
+              child: CircularProgressIndicator(),
+            ),
+            errorWidget: (context, url, error) => const Icon(Icons.error),
           ),
-          placeholder: (context, url) => const Center(
-            child: CircularProgressIndicator(),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                children: [
+                  Text(
+                    currentUser.displayName ?? 'John Doe',
+                    style: kBold18,
+                  ),
+                  const SizedBox(
+                    width: 10,
+                  ),
+                  currentUser.emailVerified
+                      ? const Icon(
+                          Icons.verified,
+                          color: Colors.green,
+                        )
+                      : const SizedBox()
+                ],
+              ),
+              Text(
+                currentUser.email!,
+                style: kMedium12,
+              ),
+              const Row(
+                children: [
+                  Icon(
+                    Icons.location_on,
+                    color: Colors.red,
+                  ),
+                  SizedBox(
+                    width: 2,
+                  ),
+                  Text(
+                    'Katmandu, Nepal',
+                    style: kSemiBold14,
+                  ),
+                ],
+              ),
+            ],
           ),
-          errorWidget: (context, url, error) => const Icon(Icons.error),
-        ),
-        Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Row(
+          const Divider(),
+          const Padding(
+            padding: EdgeInsets.symmetric(vertical: 12.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
                 Text(
-                  currentUser.displayName ?? 'John Doe',
-                  style: kBold18,
-                ),
-                const SizedBox(
-                  width: 10,
-                ),
-                currentUser.emailVerified
-                    ? const Icon(
-                        Icons.verified,
-                        color: Colors.green,
-                      )
-                    : const SizedBox()
-              ],
-            ),
-            Text(
-              currentUser.email!,
-              style: kMedium12,
-            ),
-            const Row(
-              children: [
-                Icon(
-                  Icons.location_on,
-                  color: Colors.red,
-                ),
-                SizedBox(
-                  width: 2,
+                  'Occupation',
+                  style: kBold16,
                 ),
                 Text(
-                  'Katmandu, Nepal',
+                  'Software Engineer',
                   style: kSemiBold14,
                 ),
               ],
             ),
-          ],
-        ),
-        const Divider(),
-        const Padding(
-          padding: EdgeInsets.symmetric(vertical: 12.0),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Text(
-                'Occupation',
-                style: kBold16,
-              ),
-              Text(
-                'Software Engineer',
-                style: kSemiBold14,
-              ),
-            ],
           ),
-        ),
-        const Divider(),
-        const SizedBox(
-          height: 10,
-        ),
-        const Text(
-          'About Me',
-          style: kBold16,
-        ),
-        const Text(
-          'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla euismod, nisl eget ultricies ultrices, nunc nisl ultricies nunc, quis ultricies nisl nisl eget nisl. Nulla euismod, nisl eget ultricies ultrices, nunc nisl ultricies nunc, quis ultricies nisl nisl eget nisl.',
-        ),
-        const SizedBox(
-          height: 10,
-        ),
-        const Divider(),
-        const Text(
-          'My Interests',
-          style: kBold16,
-        ),
-        const SizedBox(
-          height: 10,
-        ),
-        Wrap(
-          spacing: 10,
-          runSpacing: 10,
-          children: _intrest
-              .map(
-                (e) => Chip(
-                  avatar: Icon(
-                    e.icon,
-                    color: Colors.black,
+          const Divider(),
+          const SizedBox(
+            height: 10,
+          ),
+          const Text(
+            'About Me',
+            style: kBold16,
+          ),
+          const Text(
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla euismod, nisl eget ultricies ultrices, nunc nisl ultricies nunc, quis ultricies nisl nisl eget nisl. Nulla euismod, nisl eget ultricies ultrices, nunc nisl ultricies nunc, quis ultricies nisl nisl eget nisl.',
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          const Divider(),
+          const Text(
+            'My Interests',
+            style: kBold16,
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          Wrap(
+            children: _intrest
+                .map(
+                  (intrest) => Card(
+                    child: ListTile(
+                      horizontalTitleGap: 0,
+                      leading: Icon(intrest.icon),
+                      title: Text(intrest.name),
+                    ),
                   ),
-                  label: Text(e.name),
-                  backgroundColor: Colors.grey[300],
-                ),
-              )
-              .toList(),
-        ),
-      ],
+                )
+                .toList(),
+          ),
+        ],
+      ),
     );
   }
 }
