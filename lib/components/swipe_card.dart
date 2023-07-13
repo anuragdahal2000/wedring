7ef06c66e04e59b1c231cbc3af62b0fd4a511390
abhishekabh <abhishekbhusal05@gmail.com>
2023-07-13 20:58:35 +0545

    swipe user home

---
+++ lib/components/swipe_card.dart
@@ -0,0 +1,144 @@
+/// The swiping_card_deck library contains the [SwipingCardDeck] widget
+/// which allows a user to swipe through a deck of [Card] widgets. To
+/// swipe through any [Widget], use the generic [SwipingDeck] class.
+library swiping_card_deck;
+
+import 'package:flutter/material.dart';
+import 'package:wedring/components/swipe_gesture.dart';
+
+/// A [SwipingDeck] of [Card] widgets
+typedef SwipingCardDeck = SwipingDeck<Card>;
+
+/// A deck of [Widget] objects that can be swiped to the left or right
+/// using a gesture or a button.
+// ignore: must_be_immutable
+class SwipingDeck<T extends Widget> extends StatelessWidget {
+  SwipingDeck(
+      {Key? key,
+      required this.cardDeck,
+      required this.onLeftSwipe,
+      required this.onRightSwipe,
+      required this.onDeckEmpty,
+      required this.cardWidth,
+      this.minimumVelocity = 1000,
+      this.rotationFactor = .8 / 3.14,
+      this.swipeThreshold,
+      this.swipeAnimationDuration = const Duration(milliseconds: 500),
+      this.disableDragging = false})
+      : super(key: key) {
+    cardDeck = cardDeck.reversed.toList();
+  }
+
+  /// The list of [Widget] objects to be swiped.
+  List<T> cardDeck;
+
+  /// Callback function ran when a [Widget] is swiped left.
+  final Function(T) onLeftSwipe;
+
+  /// Callback function ran when a [Widget] is swiped right.
+  final Function(T) onRightSwipe;
+
+  /// Callback function when the last [Widget] in the [cardDeck] is swiped.
+  final Function() onDeckEmpty;
+
+  /// The minimum horizontal velocity required to trigger a swipe.
+  final double minimumVelocity;
+
+  /// The amount each [Widget] rotates as it is swiped.
+  final double rotationFactor;
+
+  /// The width of all [Widget] objects in the [cardDeck].
+  final double cardWidth;
+
+  /// The [Duration] of the swiping [AnimationController].
+  final Duration swipeAnimationDuration;
+
+  /// Disable swiping cards using the dragging gesture.
+  final bool disableDragging;
+
+  /// The distance in pixels that a [Widget] must be dragged before it is swiped.
+  late final double? swipeThreshold;
+
+  /// The [SwipingGestureDetector] used to control swipe animations.
+  late final SwipingGestureDetector swipeDetector;
+
+  /// The [Size] of the screen.
+  late final Size screenSize;
+
+  bool animationActive = false;
+  static const String left = "left";
+  static const String right = "right";
+
+  @override
+  Widget build(BuildContext context) {
+    screenSize = MediaQuery.of(context).size;
+    swipeDetector = SwipingGestureDetector(
+      cardDeck: cardDeck,
+      swipeLeft: swipeLeft,
+      swipeRight: swipeRight,
+      swipeThreshold: swipeThreshold ?? screenSize.width / 4,
+      cardWidth: cardWidth,
+      rotationFactor: rotationFactor,
+      minimumVelocity: minimumVelocity,
+      swipeAnimationDuration: swipeAnimationDuration,
+      disableDragging: disableDragging,
+    );
+    return SizedBox(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          SizedBox(
+            width: MediaQuery.of(context).size.width,
+            child: swipeDetector,
+          ),
+        ],
+      ),
+    );
+  }
+
+  /// Swipe the top [Widget] to the left.
+  ///
+  /// If there is no animation already in progress, trigger the animation
+  /// to swipe the top [Widget] to the left, call the function [onLeftSwipe],
+  /// and remove the [Widget] from the deck. If the deck is empty, call the
+  /// function [onDeckEmpty].
+  Future<void> swipeLeft() async {
+    if (animationActive || cardDeck.isEmpty) return;
+    await _swipeCard(left, screenSize);
+    onLeftSwipe(cardDeck.last);
+    cardDeck.removeLast();
+    if (cardDeck.isEmpty) onDeckEmpty();
+  }
+
+  /// Swipe the top [Widget] to the right.
+  ///
+  /// If there is no animation already in progress, trigger the animation
+  /// to swipe the top [Widget] to the right, call the function [onRightSwipe],
+  /// and remove the [Widget] from the deck. If the deck is empty, call the
+  /// function [onDeckEmpty].
+  Future<void> swipeRight() async {
+    if (animationActive || cardDeck.isEmpty) return;
+    await _swipeCard(right, screenSize);
+    onRightSwipe(cardDeck.last);
+    cardDeck.removeLast();
+    if (cardDeck.isEmpty) onDeckEmpty();
+  }
+
+  Future<void> _swipeCard(String direction, Size screenSize) async {
+    double swipeEnd = screenSize.width / 2 + cardWidth;
+    swipeDetector.swipe = AlignmentTween(
+      begin: swipeDetector.dragAlignment,
+      end: Alignment(direction == left ? -swipeEnd : swipeEnd,
+          swipeDetector.dragAlignment.x),
+    ).animate(
+      CurvedAnimation(
+        parent: swipeDetector.swipeController,
+        curve: Curves.ease,
+      ),
+    );
+    animationActive = true;
+    await swipeDetector.swipeController.forward();
+    swipeDetector.swipeController.reset();
+    animationActive = false;
+  }
+}
---
+++ lib/components/swipe_gesture.dart
@@ -0,0 +1,183 @@
+// ignore_for_file: must_be_immutable
+
+import 'dart:math';
+import 'package:flutter/material.dart';
+import 'package:flutter/physics.dart';
+
+class SwipingGestureDetector<T> extends StatefulWidget {
+  SwipingGestureDetector({
+    Key? key,
+    required this.cardDeck,
+    required this.swipeLeft,
+    required this.swipeRight,
+    required this.cardWidth,
+    this.minimumVelocity = 1000,
+    this.rotationFactor = .8 / 3.14,
+    this.swipeAnimationDuration = const Duration(milliseconds: 500),
+    required this.swipeThreshold,
+    this.disableDragging = false,
+  }) : super(key: key);
+
+  final List<T> cardDeck;
+  final Function() swipeLeft, swipeRight;
+  final double minimumVelocity;
+  final double rotationFactor;
+  final double swipeThreshold;
+  final double cardWidth;
+  final Duration swipeAnimationDuration;
+  final bool disableDragging;
+
+  late Alignment dragAlignment = Alignment.center;
+
+  late final AnimationController swipeController;
+  late Animation<Alignment> swipe;
+
+  @override
+  State<StatefulWidget> createState() => _SwipingGestureDetector();
+}
+
+class _SwipingGestureDetector extends State<SwipingGestureDetector>
+    with TickerProviderStateMixin {
+  bool animationActive = false;
+  late final AnimationController springController;
+  late Animation<Alignment> spring;
+
+  @override
+  void initState() {
+    super.initState();
+    springController = AnimationController(vsync: this);
+    springController.addListener(() {
+      setState(() {
+        widget.dragAlignment = spring.value;
+      });
+    });
+
+    widget.swipeController = AnimationController(
+        vsync: this, duration: widget.swipeAnimationDuration);
+    widget.swipeController.addListener(() {
+      setState(() {
+        widget.dragAlignment = widget.swipe.value;
+      });
+    });
+  }
+
+  @override
+  void didUpdateWidget(covariant SwipingGestureDetector oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    widget.swipeController = oldWidget.swipeController;
+  }
+
+  @override
+  void dispose() {
+    springController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final Size screenSize = MediaQuery.of(context).size;
+    return GestureDetector(
+      onPanUpdate: !widget.disableDragging ? _onPanUpdate : (_) {},
+      onPanStart: !widget.disableDragging ? _onPanStart : (_) {},
+      onPanEnd: !widget.disableDragging
+          ? (details) => _onPanEnd(details, screenSize)
+          : (_) {},
+      child: Stack(
+        alignment: Alignment.center,
+        children: topTwoCards(),
+      ),
+    );
+  }
+
+  void _onPanUpdate(DragUpdateDetails details) {
+    setState(() {
+      widget.dragAlignment += Alignment(details.delta.dx, details.delta.dy);
+    });
+  }
+
+  void _onPanStart(DragStartDetails details) async {
+    if (animationActive) {
+      springController.stop();
+    }
+  }
+
+  void _onPanEnd(DragEndDetails details, Size screenSize) async {
+    double vx = details.velocity.pixelsPerSecond.dx;
+    if (vx >= widget.minimumVelocity ||
+        widget.dragAlignment.x >= widget.swipeThreshold) {
+      await widget.swipeRight();
+    } else if (vx <= -widget.minimumVelocity ||
+        widget.dragAlignment.x <= -widget.swipeThreshold) {
+      await widget.swipeLeft();
+    } else {
+      animateBackToDeck(details.velocity.pixelsPerSecond, screenSize);
+    }
+    setState(() {
+      widget.dragAlignment = Alignment.center;
+    });
+  }
+
+  List<Widget> topTwoCards() {
+    if (widget.cardDeck.isEmpty) {
+      return [
+        const SizedBox(
+          height: 0,
+          width: 0,
+        )
+      ];
+    }
+    List<Widget> cardDeck = [];
+    int deckLength = widget.cardDeck.length;
+    for (int i = max(deckLength - 2, 0); i < deckLength; ++i) {
+      cardDeck.add(widget.cardDeck[i]);
+    }
+    Widget topCard = cardDeck.last;
+    cardDeck.removeLast();
+    cardDeck.add(
+      Align(
+          alignment: Alignment(getCardXPosition(), 0),
+          child: Transform.rotate(
+            angle: getCardAngle(),
+            child: topCard,
+          )),
+    );
+    return cardDeck;
+  }
+
+  double getCardAngle() {
+    final double screenWidth = MediaQuery.of(context).size.width;
+    return widget.rotationFactor * (widget.dragAlignment.x / screenWidth);
+  }
+
+  double getCardXPosition() {
+    final double screenWidth = MediaQuery.of(context).size.width;
+    return widget.dragAlignment.x / ((screenWidth - widget.cardWidth) / 2);
+  }
+
+  void animateBackToDeck(Offset pixelsPerSecond, Size size) async {
+    spring = springController.drive(
+      AlignmentTween(
+        begin: widget.dragAlignment,
+        end: Alignment.center,
+      ),
+    );
+
+    // Calculate the velocity relative to the unit interval, [0,1],
+    // used by the animation controller.
+    final unitsPerSecondX = pixelsPerSecond.dx / size.width;
+    final unitsPerSecondY = pixelsPerSecond.dy / size.height;
+    final unitsPerSecond = Offset(unitsPerSecondX, unitsPerSecondY);
+    final unitVelocity = unitsPerSecond.distance;
+
+    const springProps = SpringDescription(
+      mass: 30,
+      stiffness: 1,
+      damping: 1,
+    );
+
+    final simulation = SpringSimulation(springProps, 0, 1, -unitVelocity);
+    animationActive = true;
+    await springController.animateWith(simulation);
+    animationActive = false;
+  }
+}
